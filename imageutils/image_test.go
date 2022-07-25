f67f4e7f38dcf60bbc630dfc2278de4ed5a1bddc
windealli <windealli@tencent.com>
2022-07-25 10:53:48 +0800

    feat: imageutils 添加测试用例

---
+++ imageutils/image_test.go
@@ -0,0 +1,77 @@
+package imageutils
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestGetImageTypeBySuffix(t *testing.T) {
+	type args struct {
+		b        *[]byte
+		filename string
+	}
+	tests := []struct {
+		name string
+		args args
+		want MIME
+	}{
+		// TODO: Add test cases.
+		{
+			name: "./testfile/bmp.bmp",
+			args: args{
+				b: nil,
+			},
+			want: ImageJPEG,
+		},
+		{
+			name: "./testfile/gif.gif",
+			args: args{
+				b: nil,
+			},
+			want: ImageGIF,
+		},
+		{
+			name: "./testfile/jpeg.jpg",
+			args: args{
+				b: nil,
+			},
+			want: ImageJPEG,
+		},
+		{
+			name: "./testfile/png.png",
+			args: args{
+				b: nil,
+			},
+			want: ImagePNG,
+		},
+		{
+			name: "./testfile/tiff.tif",
+			args: args{
+				b: nil,
+			},
+			want: ImageTIF,
+		},
+	}
+	for _, tt := range tests {
+
+		fp, err := os.OpenFile(tt.name, os.O_CREATE|os.O_APPEND, 6) // 读写方式打开
+		if err != nil {
+			// 如果有错误返回错误内容
+			t.Errorf(err.Error())
+		}
+		//defer fp.Close()
+
+		b, err := ioutil.ReadAll(fp)
+		if err != nil {
+			t.Errorf(err.Error())
+		}
+
+		t.Run(tt.name, func(t *testing.T) {
+			if got := GetImageTypeBySuffix(&b); got != tt.want {
+				t.Errorf("GetImageTypeBySuffix() = %v, want %v", got, tt.want)
+			}
+		})
+
+	}
+}
